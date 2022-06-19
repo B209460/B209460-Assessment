b6d06ebe86571a341473075a9f6444fc46d48092
B206490 <E.Vincent-4@sms.ed.ac.uk>
2022-06-19 21:22:04 +0000

    Subset of AE attendance data set for training Data Capture tool

---
+++ RScripts/Creating a data dictionary.R
@@ -0,0 +1,34 @@
+remove.packages(here)
+install.packages("here")
+library(here)
+
+remove.packages(dataMeta)
+
+install.packages("dataMeta")
+library(dataMeta)
+library (tidyverse)
+
+knitr::opts_chunk$set(echo = TRUE)
+
+CollectedData=read_csv(here("B209460-Assessment", "RawData", "CollectedDataFinal.csv"))
+
+glimpse(CollectedData) 
+
+variable_description <- c("The index column that allows us to link the data collected to the original ae_attendances data in the 'RawData' folder.",
+                          "The month that this activity relates to, stored as a date (1st of each month).",
+                          "The number of attendances for this department type at this organisation for this month.", 
+                          "The number of attendances that breached the four-hour target.", 
+                          "The Organisation data service (ODS) code for the organisation. If you want to know the organisation associated with a particular ODS code, you can look it up from the following address: https://odsportal.digital.nhs.uk/Organisation/Search.", 
+                          "The consent from the end-user to process and share the data collected with the data capture tool.")
+print(variable_description)
+
+variable_type <- c(0, 1, 0, 0, 1, 1)
+
+print(variable_type)
+linker<-build_linker(CollectedData, variable_description, variable_type)
+print(linker)
+
+dictionary <- build_dict(my.data = CollectedData, linker = linker)
+
+glimpse(dictionary)
+
